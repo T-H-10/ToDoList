10e2962fd0055b0d17b801b434cfed8a16e8539c
T-H-10 <t0534198344@gmail.com>
2025-02-06 02:10:07 +0200

    add

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d825bdaf9b6197856f7a22a02ea2c1266de863d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaf913837f1c5d1b336bc7b8006f97c69c3f42cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
